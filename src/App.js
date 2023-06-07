20a52d2ae1c2d5644233effd825d1609c42a983c
CreatorAswin <kumarikumar235@gmail.com>
2023-06-07 11:25:22 +0530

    second commit

--- src/App.js
+++ src/App.js
@@ -1,10 +1,42 @@
 import React from 'react';
+import Header from './Header';
+
+const PrintName =()=>{
+  return <h2>Hello Aswin</h2>
+}
 
 const App = () => {
   return (
-    <div><h1>Time {new Date().toLocaleTimeString()}</h1></div>
+   <section>
+    <Movie/>
+    <Movie/>
+    <Movie/>
+    <Movie/>
+   </section>
   )
 }
 
+
+const Movie = () => {
+  return (
+    <div className='movie'>
+      <ImageComponent/>
+      <ImageTitle />
+    </div>
+  )
+};
+
+const ImageComponent=()=>{
+  return(
+    <section>
+      <img style={{width:'30%', padding:'20px 0 0 10px'}} src='https://static-koimoi.akamaized.net/wp-content/new-galleries/2023/02/lost-movie-review-1.jpg' alt='movie'/>
+    </section>
+  )
+};
+const ImageTitle =()=>{
+  return (
+  <h2 style={{color:"red",fontSize:'20px',margin:'5px',paddingLeft:'20px'}}>This is First Movie</h2>
+)
+};
 
 export default App;
--- src/Header.js
+++ src/Header.js
@@ -1,8 +1,11 @@
 import React from 'react';
 
+
+const name = "Welcome to React";
+
 const Header = () => {
   return (
-    <div>Header</div>
+    <h1>{name}</h1>
   )
 }
 export default Header;
